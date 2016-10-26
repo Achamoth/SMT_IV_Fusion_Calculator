b87f1776b06dbac14e69881c6a2b74b4ce293dd2
Achamoth <21521274@student.uwa.edu.au>
2016-10-27 00:36:35 +0800

    Started Apocalypse
    Started work on apocalypse.
    
    Split directories; one for SMT IV, one for SMT IV Apocalypse
    
    All the code should be the same, except for Race, which has already
    been changed (only slightly) to match Apocalypse’s new races
    
    Am in the middle of completing Apocalypse’s demon databases. Have
    finished up to and including Fury. Should continue off Genma
    
    Also need to update the special fusions file for Apocalypse
    
    Need to confirm that the fusion rules are the same for Apocalypse. If
    so, everything inside the Rules folder can remain untouched (have
    already removed Godly and Chaos from fusion chart file)

---
+++ Apocalypse/src/Race.java
@@ -0,0 +1,231 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.logging.*;
+import java.io.IOException;
+import java.lang.NullPointerException;
+
+public enum Race {
+  Deity("deity"),
+  Amatsu("amatsu"),
+  Megami("megami"),
+  Nymph("nymph"),
+  Enigma("enigma"),
+  Entity("entity"),
+  Primal("primal"),
+  Fury("fury"),
+  Kunitsu("kunitsu"),
+  Kishin("kishin"),
+  Zealot("zealot"),
+  Lady("lady"),
+  Reaper("reaper"),
+  Vile("vile"),
+  Tyrant("tyrant"),
+  Genma("genma"),
+  Yoma("yoma"),
+  Fairy("fairy"),
+  Night("night"),
+  Herald("herald"),
+  Divine("divine"),
+  Fallen("fallen"),
+  Avian("avian"),
+  Flight("flight"),
+  Raptor("raptor"),
+  Jirae("jirae"),
+  Brute("brute"),
+  Femme("femme"),
+  Jaki("jaki"),
+  Dragon("dragon"),
+  Snake("snake"),
+  Drake("drake"),
+  Avatar("avatar"),
+  Holy("holy"),
+  Food("food"),
+  Beast("beast"),
+  Wilder("wilder"),
+  Tree("tree"),
+  Wood("wood"),
+  Ghost("ghost"),
+  Spirit("spirit"),
+  Undead("undead"),
+  Vermin("vermin"),
+  Foul("foul"),
+  Element("element"),
+  Fiend("fiend"),
+  Famed("famed");
+
+  private List<String[]> fusionCombination;
+  private List<String> elementsUp;
+  private List<String> elementsDown;
+  private List<Demon> demons;
+  private List<Demon> compendium;
+  private Map<String, Demon> stringToDemon;
+  private Map<String, Demon> stringToDemonCompendium;
+
+  //Static string to enum map
+  private static Map<String, Race> stringToEnum = new HashMap<String, Race>();
+  //Flag that determines whether or not compendium demons are used in computation
+  private static boolean useCompendium = true;
+
+  static {
+    //Populate string to enum map
+    for(Race r : values()) {
+      stringToEnum.put(r.toString().toLowerCase(), r);
+    }
+  }
+
+  Race(String race) {
+    //Populate all data for race
+    fusionCombination = new ArrayList<String[]>();
+    elementsUp = new ArrayList<String>();
+    elementsDown = new ArrayList<String>();
+    demons = new ArrayList<Demon>();
+    compendium = new ArrayList<Demon>();
+    FileOps.populateRaceData(race, fusionCombination, elementsUp, elementsDown, demons, compendium);
+    //Populate string to demon map for race
+    stringToDemon = new HashMap<String, Demon>();
+    for(Demon d : demons) {
+      stringToDemon.put(d.getName(), d);
+    }
+    //Populate string to demon map for compendium demons in race
+    stringToDemonCompendium = new HashMap<String, Demon>();
+    for(Demon d : compendium) {
+      stringToDemonCompendium.put(d.getName(), d);
+    }
+  }
+
+  public static void useCompendiumDemons() {
+    useCompendium = true;
+  }
+
+  public static void dontUseCompendiumDemons() {
+    useCompendium = false;
+  }
+
+  /**
+    *Returns a race enum, given a string name for the desired race
+    *@param s the string representation of the race. MUST BE LOWER CASE
+    *return the race enum corresponding to the string representation
+    */
+  public static Race fromString(String s) {
+    return stringToEnum.get(s);
+  }
+
+  public List<String[]> getFusionComponents() {
+    return this.fusionCombination;
+  }
+
+  public List<String> getElementsUp() {
+    return this.elementsUp;
+  }
+
+  public List<String> getElementsDown() {
+    return this.elementsDown;
+  }
+
+  public List<Demon> getDemons() {
+    //Create a copy of the list
+    List<Demon> result = new ArrayList<Demon>();
+    for(Demon d : this.demons) {
+      result.add(new Demon(d));
+    }
+    return result;
+  }
+
+  /**
+    * Returns boolean value signifying whether this race enum is an element or not
+    * @return true if race is element, false otherwise
+    */
+  public boolean isElement() {
+    return this.toString().equalsIgnoreCase("element");
+  }
+
+  /**
+    * Given a demon name, find and return the corresponding base demon
+    * @param name the desired demon's name
+    * @return the corresponding demon object
+    */
+  public Demon getDemon(String name) {
+    return new Demon(stringToDemon.get(name));
+  }
+
+  /**
+    * Given a demon name, find and return the corresponding non-base demon (from the compendium)
+    * If no such demon exists, return null
+    * @param name the desired demon's name
+    * @return the corresponding demon object
+    */
+    public Demon getCompendiumDemon(String name) {
+      if(stringToDemonCompendium.get(name) == null || !useCompendium) {
+        return null;
+      }
+      return new Demon(stringToDemonCompendium.get(name));
+    }
+
+  /**
+    * Given a demon belonging to this race, return the base level of the demon
+    * @param demon the demon for which we want to find the base level
+    * @throws NullPointerException if demon is null
+    * @return the base level of the supplied demon. -1 if supplied demon couldn't be found in race's demon list
+    */
+  public int getBaseLevel(Demon demon) {
+    //Ensure demon isn't null
+    if(demon == null)  {
+      //throw new NullPointerException("Null-pointer passed as parameter to getBaseLevel()");
+    }
+    //Get demon's name
+    String name = demon.getName();
+    //Get base demon off demon map
+    Demon base = this.stringToDemon.get(name);
+    if(base == null) {
+      return -1;
+    }
+    return base.getLevel();
+  }
+
+  public List<Demon> getWeakerDemons(Demon demon) {
+    //Create empty list to store results
+    List<Demon> result = new ArrayList<Demon>();
+    //Get supplied demon's base level
+    int baseLevel = this.getBaseLevel(demon);
+    //Loop through all demons in race's demon list
+    for(Demon cur : this.demons) {
+      if(cur.getLevel() < baseLevel) {
+        result.add(cur);
+      }
+      else {
+        break;
+      }
+    }
+    //Return list
+    return result;
+  }
+
+  public List<Demon> getStrongerDemons(Demon demon) {
+    //Create empty list to store results
+    List<Demon> result = new ArrayList<Demon>();
+    //Get supplied demon's base level
+    int baseLevel = this.getBaseLevel(demon);
+    //Loop through all demons in race's demon list in reverse order
+    for(int i=this.demons.size()-1; i>=0; i--) {
+      Demon cur = this.demons.get(i);
+      if(cur.getLevel() > baseLevel) {
+        result.add(cur);
+      }
+      else {
+        break;
+      }
+    }
+    return result;
+  }
+
+  public Demon getWeakestDemon() {
+    return this.demons.get(0);
+  }
+
+  public Demon getStrongestDemon() {
+    return this.demons.get(demons.size()-1);
+  }
+
+}
---
+++ Apocalypse/src/Runner.java
@@ -0,0 +1,104 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.util.TreeSet;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.logging.*;
+import java.util.Scanner;
+import java.io.IOException;
+import java.lang.NumberFormatException;
+import java.lang.NullPointerException;
+
+public class Runner {
+
+  private static Map<String, SpecialFusion> specialFusions = new HashMap<String, SpecialFusion>();
+  private static Logger logger = Logger.getLogger("SpecialFusion");
+
+  public static void main(String[] args) {
+    try {
+      //Try to set up logger file handler
+      logger.setLevel(Level.ALL);
+      Handler handler = new FileHandler("../logs/Runner.log");
+      logger.addHandler(handler);
+      //First, read all fusion rules
+      FileOps.readFusionChart();
+      //Next, read all special fusions
+      FileOps.populateSpecialFusions(specialFusions);
+    }
+    catch (IOException e) {
+
+    }
+    catch (NullPointerException e) {
+
+    }
+    catch(NumberFormatException e) {
+
+    }
+
+    //Set up scanner to take user input
+    Scanner in = new Scanner(System.in);
+
+    //Ask user for desired demon's details
+    System.out.print("Would you like to use the compendium(y/n): ");
+    String compendiumUsage = in.nextLine().trim();
+    if(compendiumUsage.equals("y")) {
+      Race.useCompendiumDemons();
+      System.out.print("What skill threshold would you like for evaluating compendium demons: ");
+      int skillThreshold = Integer.valueOf(in.nextLine().trim());
+      Fusion.setSkillThreshold(skillThreshold);
+    }
+    else {
+      Race.dontUseCompendiumDemons();
+    }
+
+    System.out.print("Enter the desired depth limit: ");
+    // int desiredDepthLimit = in.nextInt();
+    int desiredDepthLimit = Integer.valueOf(in.nextLine().trim());
+    Fusion.setDepthLimit(desiredDepthLimit);
+
+    System.out.print("Enter the desired demon's race: ");
+    String desiredRace = in.nextLine().trim();
+
+    System.out.print("Enter the desired demon's name: ");
+    String desiredDemon = in.nextLine().trim();
+
+    System.out.print("Enter the desired demon's level: ");
+    int level = in.nextInt();
+
+    System.out.print("How many skills would you like the demon to have: ");
+    int numSkills = in.nextInt();
+
+    System.out.println("Enter the desired skills:");
+    in.nextLine();
+    Set<String> skills = new HashSet<String>();
+    for(int i=0; i<numSkills; i++) skills.add(in.nextLine().trim());
+    System.out.println();
+
+    System.out.print("How many fusion chains do you want: ");
+    int numChains = Integer.valueOf(in.nextLine().trim());
+
+    //Create demon object
+    Demon desired = new Demon(desiredRace, desiredDemon, level, skills);
+
+    //Find fusion chains for demon
+    List<FusionChain> allChains = Fusion.findFusionChains(desired, numChains);
+    //Get rid of duplicates
+    Set<FusionChain> recipes = new HashSet<FusionChain>();
+    for(FusionChain curChain : allChains) {
+      recipes.add(curChain);
+    }
+    //Print fusion chains
+    System.out.println();
+    System.out.println("Fusing: " + desired.getRace().toString().substring(0,1).toUpperCase()+desired.getRace().toString().substring(1)+" "+desired.getName());
+    System.out.println("Skills: " + skills.toString()+"\n");
+    System.out.println();
+    for(FusionChain recipe : recipes) {
+      System.out.println();
+      recipe.printChain(0,skills);
+      System.out.println();
+    }
+    System.out.println();
+  }
+}
